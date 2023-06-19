a8d85f8a0bd61f10b71992399c053775f313d5b7
Travis Gomez <frank.travis.gomez@gmail.com>
2023-06-19 10:17:52 -0500

    new commit all good

---
+++ src/main/java/com/example/ftgresume/FtgResumeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ftgresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtgResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtgResumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ftgresume/FtgResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ftgresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtgResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
